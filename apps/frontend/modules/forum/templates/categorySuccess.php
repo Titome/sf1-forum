a960f49d53a4a64bae018ce418bd07f86fb23d9f
Hugo Hamon <hugo.hamon@sensiolabs.com>
2014-05-21 18:00:39 +0200

    [frontend] added breadcrumb

--- apps/frontend/modules/forum/templates/categorySuccess.php
+++ apps/frontend/modules/forum/templates/categorySuccess.php
@@ -1,1 +1,23 @@
+<?php use_helper('I18N', 'Number') ?>
+<?php slot('title', $category->getName()) ?>
+
 <h1><?php echo $category ?></h1>
+
+<ul class="breadcrumb">
+    <li>
+        <?php echo link_to(__('Home'), 'homepage') ?>
+    </li>
+    <li>
+        <?php echo link_to(__('Forum'), 'forum') ?>
+    </li>
+    <?php foreach ($category->getParents() as $parent) : ?>
+    <li>
+        <?php echo link_to($parent->getName(), 'forum_category', array(
+            'category' => $parent->getSlug()
+        )) ?>
+    </li>
+    <?php endforeach ?>
+    <li>
+        <?php echo $category->getName() ?>
+    </li>
+</ul>
--- lib/model/doctrine/ForumCategory.class.php
+++ lib/model/doctrine/ForumCategory.class.php
@@ -2,6 +2,29 @@
 
 class ForumCategory extends BaseForumCategory
 {
+    /**
+     * Returns the list of parent categories.
+     *
+     * @param int|null $depth The seniority depth
+     *
+     * @return ForumCategory[]
+     */
+    public function getParents($depth = null)
+    {
+        /** @var Doctrine_Node_NestedSet $node */
+        $node = $this->getNode();
+        if (!$node->hasParent()) {
+            return array();
+        }
+
+        $parents = array();
+        foreach ($node->getAncestors($depth) as $parent) {
+            $parents[] = $parent;
+        }
+
+        return $parents;
+    }
+
     public function getLastMessage()
     {
         
