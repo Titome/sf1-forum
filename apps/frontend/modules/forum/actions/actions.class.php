a1a79d0ed23fb53f30fc39ad80a0a359de6c550f
Hugo Hamon <hugo.hamon@sensiolabs.com>
2014-05-22 14:21:59 +0200

    [frontend] added threads list in each category

--- apps/frontend/modules/forum/actions/actions.class.php
+++ apps/frontend/modules/forum/actions/actions.class.php
@@ -5,9 +5,13 @@ class forumActions extends sfActions
     /** @var ForumCategoryTable */
     private $categoryTable;
 
+    /** @var ForumThreadTable */
+    private $threadTable;
+
     public function preExecute()
     {
         $this->categoryTable = ForumCategoryTable::getInstance();
+        $this->threadTable   = ForumThreadTable::getInstance();
     }
 
     public function executeIndex()
@@ -19,5 +23,12 @@ class forumActions extends sfActions
     {
         $this->category = $this->categoryTable->getCategory($request->getParameter('category'));
         $this->forward404Unless($this->category);
+
+        $this->pager  = $this->threadTable->getPaginatedThreads(
+            $request->getGetParameter('page', 1),
+            sfConfig::get('app_threads_max_per_page'),
+            $this->category
+        );
+        $this->threads = $this->pager->getResults();
     }
 }
--- apps/frontend/modules/forum/templates/categorySuccess.php
+++ apps/frontend/modules/forum/templates/categorySuccess.php
@@ -1,4 +1,4 @@
-<?php use_helper('I18N', 'Number', 'Text') ?>
+<?php use_helper('I18N', 'Number', 'Text', 'Date') ?>
 <?php slot('title', $category->getName()) ?>
 
 <h1><?php echo $category ?></h1>
@@ -17,3 +17,37 @@
         'categories' => $category->getNearestChildren(),
     )) ?>
 <?php endif ?>
+
+<h2><?php echo __('Current threads') ?></h2>
+
+<table class="table table-bordered table-stripped">
+    <thead>
+        <tr>
+            <th><?php echo __('Status') ?></th>
+            <th><?php echo __('Solved') ?></th>
+            <th><?php echo __('Discussion title') ?></th>
+            <th><?php echo __('Author') ?></th>
+            <th><?php echo __('Answers') ?></th>
+            <th><?php echo __('Last update') ?></th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($threads as $thread) : ?>
+        <tr>
+            <td>
+                <span title="<?php echo $thread->isClosed() ? 'Closed' : 'Open' ?>" 
+                      class="glyphicon glyphicon-<?php echo $thread->isClosed() ? 'lock' : 'bullhorn' ?>"></span>
+            </td>
+            <td>
+                <span title="<?php echo $thread->isSolved() ? 'Solved' : 'Unsolved' ?>" 
+                      class="glyphicon glyphicon-<?php echo $thread->isSolved() ? 'ok' : 'remove' ?>"></span>
+            </td>
+            <td><?php echo $thread->getTitle() ?></td>
+            <td><?php echo $thread->getAuthor() ?></td>
+            <td><?php echo format_number($thread->getAnswerCount()) ?></td>
+            <td><?php echo format_datetime($thread->getUpdatedAt()) ?></td>
+        </tr>
+    <?php endforeach ?>
+    </tbody>
+</table>
+
